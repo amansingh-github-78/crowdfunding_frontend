d97c5a80fc7d473e5e3c54019dcb63f12c6c6dd9
Amandeep Singh <as2041909@gmail.com>
2025-02-23 19:55:12 +0530

    Crowdfunding frontend FundMyKnowledge v0.0.2

--- src/Components/HeaderFooterHome/Navbar.jsx
+++ src/Components/HeaderFooterHome/Navbar.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import UserProfileNavbar from "./userProfileNavbar";
 
 const Navbar = () => {
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+
   return (
     <>
       <nav className="bg-blue-50 border-gray-200 dark:bg-blue-900">
@@ -10,18 +13,18 @@ const Navbar = () => {
             to="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <img src="/fmk_logo.png" className="h-8" alt="Flowbite Logo" />
+            <img src="/fmk_logo.png" className="h-8" alt="FundMyKnowledge Logo" />
             <span className="self-center lg:text-2xl md:text-2xl sm:text-sm font-semibold whitespace-nowrap text-blue-900 dark:text-white">
               FundMyKnowledge
             </span>
           </Link>
           <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
-              data-collapse-toggle="navbar-user"
               type="button"
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-blue-900 rounded-lg md:hidden hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-200 dark:text-blue-200 dark:hover:bg-blue-700 dark:focus:ring-blue-600"
               aria-controls="navbar-user"
-              aria-expanded="false"
+              aria-expanded={isUserMenuOpen}
+              onClick={() => setIsUserMenuOpen(!isUserMenuOpen)}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -42,8 +45,11 @@ const Navbar = () => {
             </button>
             <UserProfileNavbar />
           </div>
+          {/* Toggle mobile menu */}
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className={`items-center justify-between ${
+              isUserMenuOpen ? "block" : "hidden"
+            } w-full md:flex md:w-auto md:order-1`}
             id="navbar-user"
           >
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border-2 border-blue-400 rounded-lg bg-blue-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-blue-50 dark:bg-blue-900 md:dark:bg-blue-900 dark:border-blue-800">
@@ -82,53 +88,6 @@ const Navbar = () => {
                 >
                   My Campaigns
                 </Link>
-              </li>
-              <li className="md:block lg:hidden hidden ">
-                <button
-                  type="button"
-                  className="block py-2 px-3 text-blue-900 rounded-sm hover:bg-blue-600 md:hover:bg-transparent md:hover:text-indigo-600 md:p-0 dark:text-white md:dark:hover:text-indigo-300 cursor-pointer "
-                  id="campaign-menu-button"
-                  aria-expanded="false"
-                  data-dropdown-toggle="campaign-dropdown"
-                  data-dropdown-placement="bottom"
-                  aria-current="page"
-                >
-                  Campaigns
-                </button>
-                <div
-                  className="z-50 hidden text-base list-none"
-                  id="campaign-dropdown"
-                >
-                  <ul
-                    className="my-4 ml-8 flex flex-row font-medium p-4 md:p-0 border border-blue-100 rounded-lg bg-blue-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-blue-50 dark:bg-blue-900 md:dark:bg-blue-900 dark:border-blue-800"
-                    aria-labelledby="dropdownLargeButton"
-                  >
-                    <li>
-                      <Link
-                        to="/mycampaigns"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-50 dark:hover:bg-blue-700 dark:text-white"
-                      >
-                        My Campaigns
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to="/start"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-50 dark:hover:bg-blue-700 dark:text-white"
-                      >
-                        Start Campaign
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to="/explore"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-50 dark:hover:bg-blue-700 dark:text-white"
-                      >
-                        Explore Campaigns
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
               </li>
               <li>
                 <Link
